262e453090f1f01a01c9645cd26364d8d31a7f3d
DagmawiG <dgebreselasse@gmail.com>
2020-08-19 23:40:59 -0500

    Updates

---
+++ precache-manifest.252b06941647897c400ce019811dd1de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60fc63678b7aa589a07f64e45fcd0586",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "095e62d1d5c4c4e7f892",
+    "url": "/jokes/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "16d47cb3515a162ddd64",
+    "url": "/jokes/static/css/main.ea30710d.chunk.css"
+  },
+  {
+    "revision": "095e62d1d5c4c4e7f892",
+    "url": "/jokes/static/js/2.ed50d4f2.chunk.js"
+  },
+  {
+    "revision": "bd711c3eacbe61b3d19896e81170ebac",
+    "url": "/jokes/static/js/2.ed50d4f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d47cb3515a162ddd64",
+    "url": "/jokes/static/js/main.2ccdd44c.chunk.js"
+  },
+  {
+    "revision": "00b1c8f38311fb0ed306",
+    "url": "/jokes/static/js/runtime-main.76d01470.js"
+  }
+]);
