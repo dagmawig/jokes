af2f70760dcae5342f22bb8ffd864844dba0b772
DagmawiG <dgebreselasse@gmail.com>
2020-08-22 15:36:25 -0500

    Updates

---
+++ precache-manifest.3432fc6d4ea9b998b149b70ce729c817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35896a5c1fd127800db0dd468abd4933",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "5125de823ed297002573",
+    "url": "/jokes/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "b7b028d6e631646de6b3",
+    "url": "/jokes/static/css/main.ea30710d.chunk.css"
+  },
+  {
+    "revision": "5125de823ed297002573",
+    "url": "/jokes/static/js/2.25ae3850.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/jokes/static/js/2.25ae3850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b028d6e631646de6b3",
+    "url": "/jokes/static/js/main.221ef7c0.chunk.js"
+  },
+  {
+    "revision": "00b1c8f38311fb0ed306",
+    "url": "/jokes/static/js/runtime-main.76d01470.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jokes/precache-manifest.252b06941647897c400ce019811dd1de.js"
+  "/jokes/precache-manifest.3432fc6d4ea9b998b149b70ce729c817.js"
 );
 
 self.addEventListener('message', (event) => {
